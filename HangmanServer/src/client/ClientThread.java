806cd34ce526a43ca9c98b060b21817efba1b954
borrovnica <mafliato@gmail.com>
2017-08-08 19:49:07 +0200

    Setting up basic code for communication

--- HangmanServer/src/client/ClientThread.java
+++ HangmanServer/src/client/ClientThread.java
@@ -1,5 +1,49 @@
 package client;
 
-public class ClientThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class ClientThread extends Thread {
+	
+	Socket communicationSocket = null;
+	
+	//Constructor 
+	public ClientThread(Socket clientSocket) {
+		communicationSocket = clientSocket;
+	}
+	
+	//Setting up streams for communication 
+	BufferedReader clientInput = null;
+	PrintStream clientOutput = null;
+	
+	//Player info - possible to place it in an object?
+	InetAddress ip_address = communicationSocket.getInetAddress();
+	int port = communicationSocket.getPort();
+	String username="";
+	
+	@Override
+	public void run() {
+		
+		try {
+			//Initializing I/O streams
+			clientInput = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
+			clientOutput = new PrintStream(communicationSocket.getOutputStream());
+		
+			/*
+			 * logic TBA
+			 */
+			
+			//Closing communication
+			communicationSocket.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
--- HangmanServer/src/startup/Server.java
+++ HangmanServer/src/startup/Server.java
@@ -1,5 +1,44 @@
 package startup;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
 
+import client.ClientThread;
+
+public class Server {
+	
+	public static LinkedList<ClientThread> onlineUsers = new LinkedList<ClientThread>();
+
+	public static void main(String[] args) {
+		
+		int portNumber = 6666;
+		
+		try {
+			//Socket to listen for connections 
+			ServerSocket server = new ServerSocket(portNumber);
+		
+			//Socket that gets created for each client
+			Socket clientSocket = null;
+			
+			//Listening for incoming connections
+			while(true) {
+				System.out.println("Waiting for a connection...");
+			
+				//Accepting connection 
+				clientSocket = server.accept();
+				System.out.println("A connection has been made!");
+				
+				//Creating a thread for the new client
+				ClientThread newClient = new ClientThread(clientSocket);
+				newClient.start();
+			
+			
+			}
+			
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 }
